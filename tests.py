9791d0c9148d095b13fbad0d1ebc856eb6a3b53b
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-11 13:39:01 -0700

    Wrote skeleton tests.

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import microblog
+
+
+class TestWritePost(unittest.TestCase):
+    """Test the write_post function of the microblog."""
+    pass
+
+
+class TestReadPosts(unittest.TestCase):
+    """Test the read_posts function of the microblog."""
+    pass
